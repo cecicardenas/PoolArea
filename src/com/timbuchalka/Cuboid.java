c81d77a622e373aec41adbda405ed80713c6c614
Ceci Cardenas <ceciliacrdz@hotmail.com>
2020-03-04 09:59:54 -0600

    .
    It is a small Java application that calculates the volume of a cube-shaped pool

---
+++ src/com/timbuchalka/Cuboid.java
@@ -0,0 +1,21 @@
+package com.timbuchalka;
+
+public class Cuboid extends Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height;
+        if (this.height < 0) {
+            this.height = 0;
+        }
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return this.height * getArea();
+    }
+}
